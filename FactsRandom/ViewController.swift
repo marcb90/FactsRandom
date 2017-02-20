8801024dd8944c891f6551c01d03fee9e20a868c
Marc Biggar <marcbiggar@Marcs-MacBook-Air.local>
2017-02-20 21:50:12 +0000

    created buttons and labels, along with fact model

--- FactsRandom/ViewController.swift
+++ FactsRandom/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let factModel = FactModel()
+    
+    @IBOutlet weak var FactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        FactLabel.text = factModel.facts[0]
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+   
+    @IBAction func showFact() {
+        FactLabel.text = factModel.facts[1]
+        
+    }
+    
 
 }
 
