6d9dbb0201cc1cedada566bdf5d03ca0603547e6
Marc Biggar <marcbiggar@Marcs-MacBook-Air.local>
2017-02-20 21:24:58 +0000

    Initial Commit

---
+++ FactsRandom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FactsRandom
+//
+//  Created by Marc Biggar on 20/02/2017.
+//  Copyright © 2017 Marc Biggar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
