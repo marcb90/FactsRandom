59729e49296711f39b3a2041c1fcac532fd11089
Marc Biggar <marcbiggar@Marcs-MacBook-Air.local>
2017-02-22 21:18:25 +0000

    Added extra colors to color module, also added over a thousand random facts

--- FactsRandom/ColorGenerator.swift
+++ FactsRandom/ColorGenerator.swift
@@ -18,6 +18,15 @@ struct ColorGenetor {
         UIColor(red: 77/255.0, green: 75/255.0, blue: 82/255.0, alpha: 1.0), //dark color
         UIColor(red: 105/255.0, green: 94/255.0, blue: 133/255.0, alpha: 1.0), //purple color
         UIColor(red: 85/255.0, green: 176/255.0, blue: 112/255.0, alpha: 1.0), //green color
+        UIColor(red: 63/255.0, green: 120/255.0, blue: 168/255.0, alpha: 1.0),
+        UIColor(red: 73/255.0, green: 150/255.0, blue: 118/255.0, alpha: 1.0),
+        UIColor(red: 13/255.0, green: 62/255.0, blue: 168/255.0, alpha: 1.0),
+        UIColor(red: 63/255.0, green: 50/255.0, blue: 89/255.0, alpha: 1.0),
+        UIColor(red: 163/255.0, green: 12/255.0, blue: 68/255.0, alpha: 1.0),
+        UIColor(red: 33/255.0, green: 90/255.0, blue: 168/255.0, alpha: 1.0),
+        UIColor(red: 168/255.0, green: 171/255.0, blue: 105/255.0, alpha: 1.0),
+        UIColor(red: 97/255.0, green: 45/255.0, blue: 168/255.0, alpha: 1.0),
+        UIColor(red: 89/255.0, green: 156/255.0, blue: 121/255.0, alpha: 1.0),
     ]
     
     func randomColor() -> UIColor {
